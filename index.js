b6eba09ac6b2962d0b908f6d9b8de4392262b552
msc007 <alstjr5569@gmail.com>
2019-07-20 18:56:02 -0700

    Initial node setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on poart ${PORT}`));
