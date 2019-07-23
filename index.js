2e23b3628cfe70c7edd6941969cf613dafc5a3cb
msc007 <alstjr5569@gmail.com>
2019-07-22 19:13:13 -0700

    Axios and Cheerio added

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const URL =
+  "https://www.amazon.com/AMD-Ryzen-3700X-16-Thread-Processor/dp/B07SXMZLPK/ref=sr_1_1?keywords=ryzen+3900&qid=1563845675&s=gateway&sr=8-1";
+
+const userAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36";
+
+async function main() {
+  const response = await axios.get(URL, {
+    headers: {
+      "User-Agent": userAgent
+    }
+  });
+
+  const rawHTML = response.data;
+  const $ = cheerio.load(rawHTML);
+  const priceElement = $("#priceblock_ourprice").text();
+  console.log(priceElement);
+}
+
+main().catch(console.error);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
